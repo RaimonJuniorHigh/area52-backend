d7ec0f449d964fc8e9c8a41ce8860c3fd4cf71da
Younes <younes@air-van-younes.home>
2026-06-18 23:49:47 +0200

    fixing Database connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,13 +2,14 @@ import { Pool } from 'pg';
 import 'dotenv/config'; 
 
 const pool = new Pool({
-  user: 'postgres',          // Gebruik de user die je in GCP hebt aangemaakt
-  host: '34.178.141.108',        // Het Public IP adres uit je console
+  user: 'postgres',
   database: 'area52_db',
-  password: process.env.DB_PASSWORD, // Leest veilig je wachtwoord uit .env
+  password: process.env.DB_PASSWORD,
+  // Gebruik de socket voor Cloud Run, anders de host voor lokaal
+  host: process.env.DB_HOST || '34.178.141.108', 
   port: 5432,
   ssl: {
-    rejectUnauthorized: false // Dit lost de 'no encryption' fout op
+    rejectUnauthorized: false
   }
 });
 
